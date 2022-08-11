5ea299553d2f2208af7f13e326f5a6d77fafb725
arnartafjord <tafjordarnar@gmail.com>
2022-08-11 23:04:16 +0000

    update

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -2,45 +2,20 @@ import {
     PlasmicRootProvider,
     PlasmicComponent,
     ComponentRenderData,
-    extractPlasmicQueryData
+    extractPlasmicQueryData,
+    usePlasmicQueryData
   } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '../plasmic-init';
 
-// Statically fetch the data needed to render Plasmic pages or components.
-export async function getStaticProps() {
-  // You can pass in multiple page paths or component names.
-  const plasmicData = await PLASMIC.fetchComponentData('Header, Footer');
-  if (!plasmicData) {
-    throw new Error('No Plasmic design found');
-  }
-
-  // Cache the necessary data fetched for the page
-  const queryCache = await extractPlasmicQueryData(
-    <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData}>
-      <PlasmicComponent component={plasmicData.entryCompMetas[0].displayName} />
-    </PlasmicRootProvider>
-  );
-  return {
-    props: {
-      plasmicData,
-      queryCache
-      // ...
-    },
-
-    // Using incremental static regeneration, will invalidate this page
-    // after 300s (no deploy webhooks needed)
-    revalidate: 300
-  };
-};
-
-export default function Layout( props: { plasmicData: ComponentRenderData; queryCache?: Record<string, any> },{ children } :any  )
+export default function Layout( props: { children: React.ReactNode[] }  )
 {
+  const plasmicData = usePlasmicQueryData("layout", async () => PLASMIC.fetchComponentData("Header", "Footer"))
     return (
-      <PlasmicRootProvider loader={PLASMIC} prefetchedData={props.plasmicData} prefetchedQueryData={props.queryCache}>
+      plasmicData.data && <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData.data}>
         <div style={{display:'flex', alignItems:'stretch', flexDirection:'column'}}>
-            <PlasmicComponent component="Header" />
-            <div>{children}</div>
-        <PlasmicComponent component="Footer" />
+          <PlasmicComponent component="Header" />
+          <div>{props.children}</div>
+          <PlasmicComponent component="Footer" />
         </div>
         </PlasmicRootProvider>
     )
